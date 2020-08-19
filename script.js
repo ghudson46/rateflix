45c9c1d2a28028dba8c835c7902ca7fb70d8e94d
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-19 00:15:03 -0400

    search page is complete

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ $(document).ready(() => {
   
   axios.get("http://www.omdbapi.com/?s=" + searchText + "&type=movie&plot=full&apikey=db8b059d")
     .then(response => {
-      console.log(response.data.Search);
       let movies = response.data.Search;
       let output = '';
 
       $.each(movies, (index, movie) => {
         output += `
-          <div class="col-md-3>
-            <div class="well text-center>
+          <div class="col-md-3">
+            <div class="well text-center">
               <img src="${movie.Poster}">
               <h5>${movie.Title}</h5>
               <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
@@ -31,6 +30,12 @@ $(document).ready(() => {
       console.log(error);
     });
 
+  }
+
+  function movieSelected(id) {
+    sessionStorage.set("movieId: ", id);
+    window.location = 'movie.html';
+    return false;
   }
 
 });
